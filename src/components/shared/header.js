ea9c21501d1a0c60abe831ddddcf2b2cda9334a0
Ryan Burgett <ryan@burgettdev.net>
2020-03-29 17:49:30 -0400

    Remove header bottom border on iOS

--- src/components/shared/header.js
+++ src/components/shared/header.js
@@ -5,42 +5,50 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import StatusBar from './statusBar';
+import Platform from '../../modules/platform';
 
 const Header = ({ children, navigation, darkMode, hideBack = false, rightButtonIcon = '', showMenuButton = false, rightButtonIconStyle = {}, onRightButtonPress }) => {
 
   const headerBackgroundColor = darkMode ? colors.PRIMARY_DM : colors.PRIMARY_TEXT;
 
-  const headerTextStyle = {
-    color: darkMode ? colors.PRIMARY_TEXT_DM : colors.CONTAINER
+  const styles = {
+    header: {
+      backgroundColor: headerBackgroundColor
+    },
+    headerText: {
+      color: darkMode ? colors.PRIMARY_TEXT_DM : colors.CONTAINER
+    }
   };
 
+  if(Platform.isIOS()) styles.header.borderBottomWidth = 0;
+
   return (
-    <NBHeader style={{backgroundColor: headerBackgroundColor}}>
+    <NBHeader style={styles.header}>
       <StatusBar />
       <Left>
         {!hideBack ?
           <Button transparent onPress={() => navigation.goBack()}>
-            <Icon style={headerTextStyle}>arrow-back</Icon>
+            <Icon style={styles.headerText}>arrow-back</Icon>
           </Button>
           :
           null
         }
       </Left>
       <Body>
-        <Title style={headerTextStyle}>{children}</Title>
+        <Title style={styles.headerText}>{children}</Title>
       </Body>
       {rightButtonIcon || showMenuButton ?
         <Right>
           {rightButtonIcon ?
             <Button transparent onPress={onRightButtonPress}>
-              <Icon style={[headerTextStyle, rightButtonIconStyle]}>{rightButtonIcon}</Icon>
+              <Icon style={[styles.headerText, rightButtonIconStyle]}>{rightButtonIcon}</Icon>
             </Button>
             :
             null
           }
           {showMenuButton ?
             <Button transparent onPress={() => navigation.push(routes.MENU)}>
-              <Icon style={headerTextStyle}>menu</Icon>
+              <Icon style={styles.headerText}>menu</Icon>
             </Button>
             :
             null
